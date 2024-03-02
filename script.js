7b6e2240dce879cb8dd6dbc968f350ada64cdbeb
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-03-02 10:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function appendToDisplay(value) {
     function removeLastCharacter() {
       var displayValue = document.getElementById('display').value;
       document.getElementById('display').value = displayValue.slice(0, -1);
-    }
-
+    } 
+ 
     function calculate() {
       var displayValue = document.getElementById('display').value;
       var result = eval(displayValue);
