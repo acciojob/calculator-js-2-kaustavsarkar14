5964bcd45fadf04af3ce805afb563d6acf32d229
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-03-02 10:50:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function appendToDisplay(value) {
+      document.getElementById('display').value += value;
+    }
+
+    function clearDisplay() {
+      document.getElementById('display').value = '';
+    }
+
+    function removeLastCharacter() {
+      var displayValue = document.getElementById('display').value;
+      document.getElementById('display').value = displayValue.slice(0, -1);
+    }
+
+    function calculate() {
+      var displayValue = document.getElementById('display').value;
+      var result = eval(displayValue);
+      document.getElementById('display').value = result;
+    }
